07455859a4916cec1b5ffc2068efc328c300c2d9
BillNyeTheScienceGuy <BillNyeTheScienceGuy@users.noreply.github.com>
2015-07-09 07:29:33 -0800

    Create markov_chain.cpp

---
+++ markov_chain.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+#define SIZE 1000
+
+void main() {
+	char input[SIZE];
+	string s[SIZE];
+	int last_i;
+	
+	cout << "Input string: ";
+	cin >> input;
+
+	for (int i = 0; i < SIZE; i++) {
+		if (input[i] == ' ') {
+
+		}
+	}
+}
